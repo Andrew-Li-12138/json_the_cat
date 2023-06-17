fa3029bdf8160a31deda56bd05035be150ef8bdf
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-16 21:42:32 -0400

    clean up the code so it only prints descprition of breed or relevant error messages

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,28 +4,22 @@ const breedName = process.argv[2];
 
 const breedFetcher = function(breed) {
   request(`${CatUrl}${breed}`, (error, response, body) => {
-    console.log(`${CatUrl}${breed}`);
     //handling error when request is not successful
     if (error) {
+      console.log('statusCode:', response && response.statusCode);
       console.log(`${CatUrl}${breed} is an invalid URL`);//telling user URL is invalid
-      console.log('error:', error); //Print the atual error message
+      console.log(error); //Print the atual error message
       return;
     }
-    //handling status code
-    console.log('statusCode:', response && response.statusCode);
     //change JSON-formatted string to JavaScript object (note in this case it returns an array with objects in)
     const data = JSON.parse(body);
     //print out error message when breed is not found
     if (data.length === 0) {
-      console.log(`breed${breed} is not found`);
+      console.log(`breed ${breed} is not found in our database`);
       return;
     }
-    //print out the entire message for the breed
-    console.log(data);
-    //confirm that the data type is changed to object JS object (note array's data type is object)
-    console.log(typeof data);
     //Access the first entry in the data array and print out the description for the use
-    console.log(data[0]["weight"]);
+    console.log(data[0]["description"]);
 
   });
 };
